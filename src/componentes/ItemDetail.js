1ad5b5875b2b1201961943bb916384526684dc97
LB Front-end <lichiborda@gmail.com>
2023-08-29 18:08:25 -0300

    primer repositorio

--- src/componentes/ItemDetail.js
+++ src/componentes/ItemDetail.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import ItemCount from './ItemCount';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-function ItemDetail({ producto }) {
+function ItemDetail(producto) {
   console.log(producto)
   return (
-    <div className="row mt-4">
-     
-    </div>
-  );
+  <div>
+       <div className="col-md-4 mb-4">
+            <div className="card">
+                <img src={producto.imagen} className="card-img-top card-img" alt={producto.sabor} />
+                <div className="card-body">
+                    <h5 className="card-title">{producto.sabor}</h5>
+                    <p className="card-text">{producto.descripcion}</p>
+                    <p className="card-text">Precio: {producto.precio}</p>
+                    <p className="card-text">Puffs: {producto.puffs}</p>
+                </div>
+            </div>
+        </div>
+  </div>
+  )
 }
 
-export default ItemDetail;
+export default ItemDetail
--- src/componentes/ItemDetailContainer.js
+++ src/componentes/ItemDetailContainer.js
@@ -1,12 +1,11 @@
-import React, { useEffect, useState } from 'react'
+import React,{useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import ItemDetail from './ItemDetail'
-import { getProductById } from './AsyncMock'
-
+import {getProductById} from './AsyncMock'
 
 function ItemDetailContainer() {
   const [producto, setProducto]= useState({})
-  const params = useParams
+  const params = useParams()
   console.log(params.id)
   useEffect(()=>{
     getProductById(params.id)
