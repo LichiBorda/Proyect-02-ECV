e4c3f5373480ce5a3f98b6d36f5070dc95e793b6
LB Front-end <lichiborda@gmail.com>
2023-08-29 18:27:23 -0300

    primer repositorio

--- src/componentes/ItemDetail.js
+++ src/componentes/ItemDetail.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-function ItemDetail(producto) {
+function ItemDetail({producto}) {
   console.log(producto)
   return (
   <div>
@@ -9,7 +9,7 @@ function ItemDetail(producto) {
             <div className="card">
                 <img src={producto.imagen} className="card-img-top card-img" alt={producto.sabor} />
                 <div className="card-body">
-                    <h5 className="card-title">{producto.sabor}</h5>
+                    <h5 className="card-title">  {producto.sabor}</h5>
                     <p className="card-text">{producto.descripcion}</p>
                     <p className="card-text">Precio: {producto.precio}</p>
                     <p className="card-text">Puffs: {producto.puffs}</p>
